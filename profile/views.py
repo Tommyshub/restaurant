ae4880b3a663e955ae405eb70e0eda1fc0b8b184
Tommy <tommy@manjaro.localdomain>
2021-04-02 20:36:16 +0200

    add update delivery information form to userprofile

--- profile/views.py
+++ profile/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 # Menu
 def profile(request):
     """ View for displaying the profile page """
+    # Get users profile
+    profile = get_object_or_404(UserProfile, user=request.user)
+    # Form for the user profile
+    form = UserProfileForm(request.POST, instance=profile)
+    # Get order history 
+    orders = profile.orders.all()
     template = 'profile/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
     
-    return render(request, template, context, 'profile/profile.html')
+    return render(request, template, context)
