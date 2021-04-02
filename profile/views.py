58c019319c3718808274a21763dfb029a8eed5b6
Tommy <tommy@manjaro.localdomain>
2021-04-02 15:17:33 +0200

    Create model for userprofile

--- profile/views.py
+++ profile/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Menu
 def profile(request):
     """ View for displaying the profile page """
-    return render(request, 'profile/profile.html')
+    template = 'profile/profile.html'
+    context = {}
+    
+    return render(request, template, context, 'profile/profile.html')
