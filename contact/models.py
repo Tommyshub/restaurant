a81b330037b0719de0739e603245dc6e9c5e156e
Tommy <tommy@manjaro.localdomain>
2021-04-08 14:34:47 +0200

    update python files for the contact app to follow pep8 rules

--- contact/models.py
+++ contact/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Model for the contact form
+
 class Contact(models.Model):
     subject = models.CharField(max_length=120, blank=False, null=True)
     name = models.CharField(max_length=120, blank=False, null=True)
     email = models.EmailField(max_length=120, blank=False)
     message = models.TextField(blank=False)
-
 
     def __str__(self):
         return self.name
--- contact/views.py
+++ contact/views.py
@@ -6,23 +6,24 @@ from .forms import ContactForm
 
 
 def contact(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			subject = form.cleaned_data['subject']
-			body = {
-			'name': form.cleaned_data['name'], 
-			'email': form.cleaned_data['email'],
-            'message': form.cleaned_data['message'],
-			}
-			message = "\n".join(body.values())
-            
-			try:
-				send_mail(subject, message, 'contact@tommybratt.se', ['contact@tommybratt.se'])
-			except BadHeaderError:
-				return HttpResponse('Invalid header found.')
-			messages.success(request, 'Message successfully sent')
-			return redirect("index")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            body = {
+                'name': form.cleaned_data['name'],
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
 
-	form = ContactForm()
-	return render(request, "contact/contact.html", {'form':form})
+            try:
+                send_mail(subject, message, 'contact@tommybratt.se',
+                          ['contact@tommybratt.se'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            messages.success(request, 'Message successfully sent')
+            return redirect("index")
+
+    form = ContactForm()
+    return render(request, "contact/contact.html", {'form': form})
