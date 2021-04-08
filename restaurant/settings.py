22f7a903a189739a5e0487c91b6b20bddfabb559
Tommy <tommy@manjaro.localdomain>
2021-04-08 19:53:50 +0200

    change static_root dir in settings

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -173,8 +173,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 """ if 'USE_AWS' in os.environ:
     # Controls how long the browser keeps the files cached  
