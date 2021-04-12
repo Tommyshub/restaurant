a7356bf2cf6bf703ad6dbb2f0583f7a479cb8dd2
Tommy <tommy@manjaro.localdomain>
2021-04-12 17:47:44 +0200

    add admin for coupon codes

--- bag/admin.py
+++ bag/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import Coupon
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'discount_percentage', 'active']
+    list_filter = ['active']
+    search_fields = ['code']
+
+
+admin.site.register(Coupon, CouponAdmin)
