05c90a6097cf227623a1b0164b7a41bdbc1ef273
Tommy B <contact@tommybratt.se>
2021-04-22 21:45:56 +0200

    add discount to the context processor and subtract it from the total

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,12 +9,15 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    discount = settings.DISCOUNT
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
+            # Subtract discount from the total
+            total = total - discount
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -28,4 +31,6 @@ def bag_contents(request):
         'product_count': product_count,
         'total': total,
     }
+    return context
+
     return context
