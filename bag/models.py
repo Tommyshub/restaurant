7a3940e826c4e50eed5f35724eab4242711479e3
Tommy B <contact@tommybratt.se>
2021-04-22 19:05:30 +0200

    add UsedCoupons model

--- bag/models.py
+++ bag/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 
 class Coupon(models.Model):
@@ -10,3 +12,12 @@ class Coupon(models.Model):
 
     def __str__(self):
         return self.code
+
+
+class UsedCoupon(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.user.username + " used " + self.coupon.name
