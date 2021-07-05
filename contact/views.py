4dc3658dd33703dad8ef232a64e9455661840bde
Tommy B <tommy@whatever>
2021-07-05 22:28:58 +0200

    update contact view for using emailjs

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
-from django.http import HttpResponse
 from .forms import ContactForm
 
 
@@ -9,25 +7,8 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
-            body = {
-                'name': form.cleaned_data['name'],
-                'email': form.cleaned_data['email'],
-                'message': form.cleaned_data['message'],
-            }
-            message = "\n".join(body.values())
-            try:
-                send_mail(subject, message, 'contact@tommybratt.se',
-                          ['contact@tommybratt.se'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            try:
-                form.save()
-            except Exception as e:
-                print(e)
-                messages.error(
-                    request, "Couldn't save the contact info in the database")
             messages.success(request, 'Message successfully sent')
-            return redirect("index")
+        else:
+            messages.error(request, 'Message could not be sent')
     form = ContactForm()
     return render(request, "contact/contact.html", {'form': form})
