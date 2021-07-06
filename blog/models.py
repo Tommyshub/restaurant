b1267fba07d71d95e33bad927ace5f9708fd3333
Tommy B <tommy@whatever>
2021-07-06 16:54:38 +0200

    create model for posting blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from profile.models import UserProfile
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    class Meta:
+        ordering = ["-publish_date"]
+
+    title = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='cars')
+    body = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
