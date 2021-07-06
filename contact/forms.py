acdfa5909f86385b3d3f4e82748df2be3ef83222
Tommy B <tommy@whatever>
2021-07-06 16:01:25 +0200

    add email validaton and required fields to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.core.validators import validate_email
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField()
-    name = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    subject = forms.CharField(max_length=100, required=True)
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not validate_email(email):
+            raise forms.ValidationError("Invalid email")
+        return email
