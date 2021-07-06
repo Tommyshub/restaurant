1bce0a4d367464eae692215d66cf1940782e2810
Tommy B <tommy@whatever>
2021-07-06 17:26:10 +0200

    connect to blog posts in blog view

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # Index
 def blog(request):
-    """ View for displaying the index page """
-    return render(request, 'blog/blog.html')
+    """ View for displaying the blog page """
+    posts = BlogPost.objects.all()
+
+    return render(request, 'blog/blog.html', {'posts': posts})
