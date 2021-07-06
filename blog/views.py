58b3eb92b1ad536c7c5af77012acdf49f885f51e
Tommy B <tommy@whatever>
2021-07-06 18:47:00 +0200

    import blog post model

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import BlogPost
 
 # Index
+
+
 def blog(request):
     """ View for displaying the blog page """
     posts = BlogPost.objects.all()
