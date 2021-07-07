1bd934af33c9113403845de920b684c875c50c5d
Tommy B <tommy@whatever>
2021-07-07 13:27:36 +0200

    remove unique requirement from title field

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class BlogPost(models.Model):
     class Meta:
         ordering = ["-publish_date"]
 
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='static/images/')
     body = models.TextField()
     publish_date = models.DateTimeField(auto_now_add=True)
