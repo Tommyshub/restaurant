ead1720060f779a75bde77bd01e57b6f5ec3861b
Tommy B <tommy@whatever>
2021-07-07 14:01:25 +0200

    change back path for image files in blog model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,6 @@ class BlogPost(models.Model):
         ordering = ["-publish_date"]
 
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to='static/images/blog/')
     body = models.TextField()
     publish_date = models.DateTimeField(auto_now_add=True)
