0cc381c66b34f34b4efc81f7e62c165e228881b4
Tommy B <tommy@whatever>
2021-07-07 12:49:49 +0200

    change path for images to same as media root setting

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,6 @@ class BlogPost(models.Model):
         ordering = ["-publish_date"]
 
     title = models.CharField(max_length=255, unique=True)
-    image = models.ImageField(upload_to='static/images/blog/')
+    image = models.ImageField(upload_to='static/images/')
     body = models.TextField()
     publish_date = models.DateTimeField(auto_now_add=True)
