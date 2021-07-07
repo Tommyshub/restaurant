c78564ced280e6d4b12011c67f76093c7706c1b2
Tommy B <tommy@whatever>
2021-07-07 15:40:37 +0200

    admins can now remove blog posts from the blog page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.blog, name='blog'),
-
+    path('remove/<pk>/', views.remove_blog_post, name='remove_blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, HttpResponse, get_object_or_404)
 from django.contrib import messages
 from .models import BlogPost
 from .forms import BlogForm
@@ -26,5 +27,24 @@ def blog(request):
         else:
             messages.error(
                 request, 'Form is not valid!')
-
     return render(request, "blog/blog.html", context)
+
+
+def remove_blog_post(request, pk):
+    """Remove the item from the shopping bag"""
+    posts = BlogPost.objects.all()
+    form = BlogForm(request.POST or None)
+    context = {'form': form, 'posts': posts}
+    if request.method == 'POST':
+        if request.user.is_superuser:
+            try:
+                post = get_object_or_404(BlogPost, pk=pk)
+                messages.success(
+                    request, f'Removed {post.title}')
+                post.delete()
+                return HttpResponseRedirect('')
+
+            except Exception as e:
+                messages.error(request, f'Error removing post: {e}')
+                return HttpResponse(status=500)
+    return render(request, 'blog/blog.html', context)
