0c5200be7bffb062ec236ce4d1c749d24a999a03
Tommy B <tommy@whatever>
2021-07-09 17:00:34 +0200

    change email validation in contact form

--- contact/forms.py
+++ contact/forms.py
@@ -5,11 +5,5 @@ from django.core.validators import validate_email
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100, required=True)
     name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(validators=[validate_email], required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if not validate_email(email):
-            raise forms.ValidationError("Invalid email")
-        return email
