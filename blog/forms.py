a7fd4e6a457336c5d75d70feea952ce9b3d673e4
Tommy B <tommy@whatever>
2021-07-09 18:30:57 +0200

    add form validation to blog form

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,14 @@ from .models import BlogPost
 class BlogForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = ('title', 'image', 'body')
+
+    def clean(self, *args, **kwargs):
+        data = self.cleaned_data
+        content = data.get('content', None)
+        if content == "":
+            content = None
+        image = data.get('image', None)
+        if content is None and image is None:
+            raise forms.ValidationError('Fields cannot be empty.')
+        return super().clean(*args, **kwargs)
