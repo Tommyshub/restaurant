69f7c51e32deb79df46ae26a53117c181506b34a
Tommy B <tommy@whatever>
2021-07-10 06:40:16 +0200

    add comments to the profile view

--- profile/views.py
+++ profile/views.py
@@ -10,17 +10,17 @@ from checkout.models import Order
 @login_required
 def profile(request):
     """ View for displaying the profile page """
-    # Get users profile
     profile = get_object_or_404(UserProfile, user=request.user)
+    # Get users profile
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
+        # Get the form instance for the profile connected to the user
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile successfully updated')
-    # Form for the user profile
     form = UserProfileForm(instance=profile)
-    # Get order history
     orders = profile.orders.all()
+    # Get all order connected to the user
     template = 'profile/profile.html'
     context = {
         'form': form,
@@ -33,10 +33,10 @@ def profile(request):
 @login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
-
+    # Get the order connected to the order number
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
-
+    # Put that order into context for rendering of the template
     return render(request, template, context)
