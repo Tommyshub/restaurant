03a2cd121df12e35a88460eb63859545a8a28677
Tommy B <tommy@whatever>
2021-07-11 11:37:39 +0200

    shorten javascript code for creating toast messages

--- contact/static/contact/js/sendEmail.js
+++ contact/static/contact/js/sendEmail.js
@@ -15,39 +15,26 @@ function sendMail(contactForm) {
         id_message.value = "";
         console.log("SUCCESS", response);
         // Create new div for content to be displayed in
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("toast");
-        newDiv.classList.add("light-green");
-        newDiv.classList.add("black-text");
-        // Success message to attach on the new div
-        const newContent = document.createTextNode("Email successfully sent!");
-        // add the text node to the newly created div
-        newDiv.appendChild(newContent);
-        // Add the new element to the parent div
-        const parentDiv = document.getElementById("parent-container");
-        parentDiv.parentNode.insertBefore(newDiv, parentDiv);
+        $(document).ready(function () {
+          $(
+            '<p id="messageParagraph" class="toast light-green black-text">Email successfully sent!</p>'
+          ).appendTo("#messageContainer");
+        });
         // Set a timeout of 4 seconds and the remove the newDiv
         setTimeout(function () {
-          if ($(newDiv).length > 0) {
-            $(newDiv).remove();
+          if ($(messageParagraph).length > 0) {
+            $(messageParagraph).remove();
           }
         }, 4000);
       },
       function (error) {
         console.log("FAILED", error);
         // Create new div for content to be displayed in
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("toast");
-        newDiv.classList.add("red");
-        newDiv.classList.add("darken-1");
-        newDiv.classList.add("black-text");
-        // Success message to attach on the new div
-        const newContent = document.createTextNode("Email could not be sent!");
-        // add the text node to the newly created div
-        newDiv.appendChild(newContent);
-        // Add the new element to the parent div
-        const parentDiv = document.getElementById("parent-container");
-        parentDiv.parentNode.insertBefore(newDiv, parentDiv);
+        $(document).ready(function () {
+          $(
+            '<p id="messageParagraph" class="toast red darken-1 black-text">Email could not be sent!</p>'
+          ).appendTo("#messageContainer");
+        });
         // Set a timeout of 4 seconds and the remove the newDiv
         setTimeout(function () {
           if ($(newDiv).length > 0) {
