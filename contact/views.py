9ca63604e9597bbe1370df3223bcf76ae9dd0be0
Tommy B <tommy@whatever>
2021-07-12 14:04:14 +0200

    remove unused import and add if form is valid to contact view

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.contrib import messages
 from .forms import ContactForm
 
 
 def contact(request):
-    if request.method == 'POST':
+    form = ContactForm(request.POST)
+    if request.method == 'POST' and form.is_valid():
         form = ContactForm(request.POST)
     form = ContactForm()
     return render(request, "contact/contact.html", {'form': form})
