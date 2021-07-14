2120291c33afc3866740ded0bca01dbc424c7247
Tommy B <tommy@whatever>
2021-07-14 12:25:45 +0200

    update validation text in validators

--- profile/validators.py
+++ profile/validators.py
@@ -5,7 +5,7 @@ import re
 def validate_alpha_numeric(value):
     if not re.compile(r'^[ 0-9a-zA-ZäåöüÄÅÖÜ]*$').match(value):
         raise ValidationError(
-            'Sorry, this field must only contain numbers and letters!')
+            'Sorry, this field can only contain numbers and letters!')
     return value
 
 
@@ -19,30 +19,30 @@ def validate_phone_number(value):
 def validate_postal_code(value):
     if not re.compile(r'^[0-9]*$').match(value):
         raise ValidationError(
-            'Sorry, the postal code must only contain numbers!.')
+            'Sorry, the postal code can only contain numbers!.')
     return value
 
 
 def validate_city(value):
     if not re.compile(r'^[ a-zA-ZäåöüÄÅÖÜ]*$').match(value):
         raise ValidationError(
-            'Sorry, the city must only contain letters!')
+            'Sorry, the city can only contain letters!')
     return value
 
 
 def validate_name(value):
     if not re.compile(r'^[ a-zA-ZäåöüÄÅÖÜ]*$').match(value):
-        raise ValidationError('Sorry, your name must only contain letters!')
+        raise ValidationError('Sorry, your name can only contain letters!')
     return value
 
 
 def validate_county(value):
     if not re.compile(r'^[ a-zA-ZäåöüÄÅÖÜ]*$').match(value):
-        raise ValidationError('Sorry, the county must only contain letters!')
+        raise ValidationError('Sorry, the county can only contain letters!')
     return value
 
 
 def validate_country(value):
     if not re.compile(r'^[ a-zA-ZäåöüÄÅÖÜ]*$').match(value):
-        raise ValidationError('Sorry, the country must only contain letters!')
+        raise ValidationError('Sorry, the country can only contain letters!')
     return value
