fa480e50f269cfbbfe290e4748b0fdaff2f71f34
Tommy B <tommy@whatever>
2021-08-31 12:07:27 +0200

    add functionality to create review view

--- review/views.py
+++ review/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def review(request):
@@ -6,6 +7,20 @@ def review(request):
     return render(request, 'review/review.html')
 
 
-def create_review(request):
+def create_review(request, product_name):
     """ View for displaying the create review page """
+    if request.method == 'POST' and user.is_authenticated:
+        form = CreateReviewForm(request.POST or None)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product_name
+            review.comment = request.POST['comment']
+            review.rating = request.POST['rating']
+            review.user = request.user
+            review.save()
+            messages.success(
+                request, f'Successfully created review for {review.product}')
+        else:
+            messages.error(
+                request, f'Error creating review for {review.product}')
     return render(request, 'review/create_review.html')
