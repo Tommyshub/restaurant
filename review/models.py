2bd058662d6daeb138d1a17dbe27c2564b193892
Tommy B <tommy@whatever>
2021-08-31 11:46:12 +0200

    add product review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from menu.models import Product
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(default=0, validators=[MinValueValidator(1),
+                                                        MaxValueValidator(5)])
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
