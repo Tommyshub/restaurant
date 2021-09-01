7684abd00169471bf40c6f30e666b61ae6f677b2
Tommy B <tommy@whatever>
2021-09-01 10:54:12 +0200

    users are now limited to one review per product

--- review/views.py
+++ review/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import ProductReviewForm
 from .models import ProductReview
+from menu.models import Product
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
@@ -15,20 +17,27 @@ def review(request):
 @login_required
 def create_review(request, product_name):
     """ View for displaying the create review page """
+    product = get_object_or_404(Product, name=product_name)
+    reviews = ProductReview.objects.filter(user=request.user, product=product)
     if request.method == 'POST':
         form = ProductReviewForm(request.POST or None)
         if form.is_valid():
             review = form.save(commit=False)
-            review.product = product_name
+            review.product = product
             review.comment = request.POST['comment']
             review.rating = request.POST['rating']
             review.user = request.user
-            review.save()
-            messages.success(
-                request, f'Successfully created review for {review.product}')
+            if not reviews:
+                review.save()
+                messages.success(
+                    request, f'Review for {product} successfully posted !')
+            else:
+                messages.error(
+                    request, f'You cannot review {product} twice!')
+            return redirect('profile')
         else:
             messages.error(
-                request, f'Error creating review for {review.product}')
+                request, f'Error creating review for {product}')
     form = ProductReviewForm(request.POST or None)
     context = {
         'form': form,
